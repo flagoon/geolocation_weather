316450c621b911cf5331719fff9b4c8ef81d9891
Paweł Kochanek <flagoon@o2.pl>
2017-10-18 14:59:28 +0200

    Added PHP and AJAX

--- find_city.php
+++ find_city.php
@@ -1,7 +1,8 @@
 <?php
 @require 'passwords.php';
 
-echo $_POST['city'];
+//echo $_POST['city'];
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, "flagoon");
 // Check connection
@@ -11,16 +12,16 @@ if ($conn->connect_error) {
 
 //set correct coding
 $conn->set_charset('utf8');
-$sql = "SELECT city FROM data WHERE city LIKE '%" . $_POST['city'] . "%'";
+$sql = "SELECT city, coord_lat, coord_lng FROM data WHERE city LIKE '%" . $_GET['city'] . "%'";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
+if ($result->num_rows > 0 && $result->num_rows < 11) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "city: " . $row["city"] . "<br>";
+        echo "<div onClick='alertMe(this)'><div class='city-result mb-1 p-2 rounded'>$row[city]</div><div class='coord_lat hidden-xs-up'>$row[coord_lat]</div><div class='coord_lng hidden-xs-up'>$row[coord_lng]</div></div>";
     }
 } else {
-    echo "0 results";
+    echo "<div class='text-warning'>Nie ma takiego miasta, lub zbyt dużo wyników. Popraw się!</div>";
 }
 
 $conn->close();
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function alertMe(content) {
+  alert(content.textContent);
+}
+
 $(document).ready(function () {
 
   function getGeoLoc() {
@@ -25,15 +29,49 @@ $(document).ready(function () {
     });
   }
 
-
-  getGeoLoc();
-
   //bootstrap adding/removing active class on links
   $('.nav-link').on('click', function () {
     $('.active').removeClass('active');
     $(this).addClass('active');
   })
 
+
+  /*
+  this function sends input value to find-city.php page and looks for similar word
+  if found, creates list of cities user can press. If result from database is 0 or 
+  too big (more than 10), it returns a warning text.
+  */
+
+  function showCities(city) {
+    if (city == '') {
+      document.getElementById('data').innerHTML = '';
+      return;
+    }
+    if (window.XMLHttpRequest) {
+      // code for IE7+, Firefox, Chrome, Opera, Safari
+      var xmlhttp = new XMLHttpRequest();
+    } else { // code for IE6, IE5
+      var xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById('data').innerHTML = this.responseText;
+      }
+    }
+    xmlhttp.open('GET', 'find_city.php?city=' + city, true);
+    xmlhttp.send();
+  }
+
+  //added event to submit button
+  $('#city-button').click(function (e) {
+    e.preventDefault();
+    //check if input is empty, if so, location is set by navigator.geolocation
+    if ($('#city-input').val() === '') {
+      getGeoLoc();
+    } else {
+      showCities($('#city-input').val());
+    }
+  });
 
   //function to find class that starts with and remove it
   // var classes = $('#sample').attr('class').split(' ');
